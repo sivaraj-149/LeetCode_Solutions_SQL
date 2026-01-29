050ff6026408f1036ebb5f106f029e818cf8c50c
MANMATHA SIVA RAJAN J <sivaraj050406@gmail.com>
2026-01-29 12:30:07 +0530

    Create SQL query to join Person and Address tables
    Add SQL query to combine data from Person and Address tables

---
+++ combine_two_tables.sql
@@ -0,0 +1,14 @@
+--Combine Two Tables
+
+--addressId is the primary key (column with unique values) for this table.
+--Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+--Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+--Return the result table in any order.
+
+select p.firstName, p.lastName, a.city, a.state
+from Person p left join Address a
+on p.personId = a.personId;
+
